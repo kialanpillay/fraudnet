24d36499de4551b1c0e4d3c6be970fbc580dbad9
Kialan Pillay <kialanpillay@gmail.com>
2021-10-29 02:03:15 +0200

    Add FeedForwardNeuralNetwork

--- models/nn.py
+++ models/nn.py
@@ -81,3 +81,30 @@ class Model3(nn.Module):
         x = F.relu(self.fc7(x))
         x = torch.sigmoid(self.output_layer(x))
         return x
+
+
+class FeedForwardNeuralNetwork(nn.Module):
+    def __init__(self, input_dim, hidden_dim, num_layers=3, batch_norm=False):
+        super(FeedForwardNeuralNetwork, self).__init__()
+        self.num_layers = num_layers
+        self.batch_norm = batch_norm
+        if batch_norm:
+            modules = [nn.Linear(input_dim, hidden_dim), nn.BatchNorm1d(hidden_dim), nn.ReLU()]
+            for i in range(num_layers - 1):
+                modules.append(nn.Linear(hidden_dim, hidden_dim))
+                modules.append(nn.BatchNorm1d(hidden_dim))
+                modules.append(nn.ReLU())
+            modules.append(nn.Linear(hidden_dim, 1))
+            modules.append(nn.Sigmoid())
+            self.layers = nn.Sequential(*modules)
+        else:
+            modules = [nn.Linear(input_dim, hidden_dim), nn.BatchNorm1d(64), nn.ReLU()]
+            for i in range(num_layers - 1):
+                modules.append(nn.Linear(hidden_dim, hidden_dim))
+                modules.append(nn.ReLU())
+            modules.append(nn.Linear(hidden_dim, 1))
+            modules.append(nn.Sigmoid())
+            self.layers = nn.Sequential(*modules)
+
+    def forward(self, x):
+        return self.layers(x)
