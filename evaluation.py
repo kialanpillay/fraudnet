01bb9e02b5b9d644b43e1abe30bf4259275981ab
Kialan Pillay <kialanpillay@gmail.com>
2021-11-05 00:17:56 +0200

    Add predictions output

--- evaluation.py
+++ evaluation.py
@@ -1,11 +1,12 @@
 import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
 import torch.utils.data
 from sklearn.metrics import balanced_accuracy_score
 
 
-def validate(model, loader):
+def validate(model, loader, save=False):
     criterion = nn.BCELoss()
     val_loss = 0.0
     val_steps = 0
@@ -27,6 +28,9 @@ def validate(model, loader):
     loss = val_loss / val_steps
     y_true = np.concatenate(y_true)
     y_pred = np.concatenate(y_pred)
+
+    if save:
+        pd.DataFrame(y_pred, columns=['Predictions']).to_csv('predictions.csv')
     return loss, {'balanced_accuracy': balanced_accuracy_score(y_true, y_pred)}
 
 
